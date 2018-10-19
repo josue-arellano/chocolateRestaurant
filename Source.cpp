4c8d6346d6f999ce5a94dcc24fe5dc4780a4d3ce
Josue Arellano <arellajosue@protonmail.com>
2018-10-19 13:28:08 -0700

    found a better version

--- Source.cpp
+++ Source.cpp
@@ -1,49 +1,47 @@
 #include <iostream>
-//882709553
-//
+
 using namespace std;
+
 int main()
 {
-	float FINGER_PRICE = 1.25;
-	float DROP_PRICE = .85;
-	float PEPPER_PRICE = .50;
-	float BEAR_PRICE = 2.75;
-	float KISS_PRICE = .35;
-
-	cout << "00000000000000000000000000000000000\n";
-	cout << "00                               00\n";
-	cout << "0 Welcome to Chocolate Restaurant 0\n";
-	cout << "00                               00\n";
-	cout << "00000000000000000000000000000000000\n";
-	cout << "Finger.....................   $" << FINGER_PRICE << endl;
-	cout << "Drops......................   $" << DROP_PRICE << endl;
-	cout << "Peppermints................   $" << PEPPER_PRICE << endl;
-	cout << "Bears......................   $" << BEAR_PRICE << endl;
-	cout << "Kisses.....................   $" << KISS_PRICE << endl;
+	cout << "00000000000000000000000000000000000\n"
+		<< "00                               00\n"
+		<< "0 Welcome to Chocolate Restaurant 0\n"
+		<< "00                               00\n"
+		<< "00000000000000000000000000000000000\n"
+		<< "Fingers....................   $1.25\n"
+		<< "Drops......................   $0.85\n"
+		<< "Peppermints................   $0.50\n"
+		<< "Bears......................   $2.75\n"
+		<< "Kisses.....................   $0.35\n";
 	int input1;
 	cout << "How many fingers?\n";
 	cin >> input1;
-	float total = input1 * FINGER_PRICE;
+	double total1 = 1.25 * input1;
 	int input2;
 	cout << "How many drops?\n";
 	cin >> input2;
-	total += input2 * DROP_PRICE;
+	double total2 = .85 * input2;
 	int input3;
 	cout << "How many peppermints?\n";
 	cin >> input3;
-	total += input3 * PEPPER_PRICE;
+	double total3 = .5 * input3;
 	int input4;
 	cout << "How many bears?\n";
 	cin >> input4;
-	total += input4 * BEAR_PRICE;
+	double total4 = 2.75 * input4;
 	int input5;
 	cout << "How many kisses?\n";
 	cin >> input5;
-	total += input5 * KISS_PRICE;
-	float tax = total * 10.25 / 100;
+	double total5 = .35 * input5;
+	double sum = total1 + total2 + total3 + total4 + total5;
+	double tax = sum * 10.25 / 100;
+	double ftotal = sum + tax;
+	int sub = sum + tax;
 	cout << "You have odered " << input1 << " fingers, " << input2 << " drops, " << input3
-		<< " peppermints, " << input4 << " bears, and " << input5 << " kisses.\nThe total for this order is $"
-		<< total + tax << " including tax." << endl;
+		<< " peppermints, " << input4 << " bears, and " << input5 << " kisses. The total for this order is $"
+		<< sub << ".";
+	cout.precision(2);
+	cout << (ftotal - sub) * 100 << " including tax." << endl;
 	return 0;
-	
 }
